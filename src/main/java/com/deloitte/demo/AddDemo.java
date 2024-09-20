8578b8310bfba7374e412d6a31a7db2db63bd093
shreyasda <shreyasshrivatsa@gmail.com>
2024-09-20 14:58:39 +0530

    Java JPA Demo with Maven Project

---
+++ src/main/java/com/deloitte/demo/AddDemo.java
@@ -0,0 +1,36 @@
+package com.deloitte.demo;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class AddDemo {
+public static void main(String[] args) {
+	SessionFactory factory = new Configuration()
+			.configure("hibernate.cfg.xml")
+			.addAnnotatedClass(Employee.class)
+			.buildSessionFactory();
+	Session session = factory.getCurrentSession();
+
+	try {
+		
+		Employee emp = new Employee("Bholu", 87.45	);
+
+		session.beginTransaction();
+		
+		session.save(emp);
+
+		session.getTransaction().commit();
+		
+		System.out.println("Done...");
+
+	} catch (Exception e) {
+		e.printStackTrace();
+	} finally {
+
+		factory.close();
+	}
+}
+}
