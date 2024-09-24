93d5de93c79bc83264bb4b82ada78c5d5228df6d
Shreyas DA <83119227+shreyasda@users.noreply.github.com>
2024-09-24 21:26:18 +0530

    Update App.java

--- src/main/java/com/deloitte/demo/App.java
+++ src/main/java/com/deloitte/demo/App.java
@@ -9,7 +9,6 @@ public class App
     public static void main( String[] args )
     {
         //GitHub URL: https://github.com/shreyasda/Deloitte-Java-Training-2024
-    	//Jenkins URL: http://localhost:8080/job/deloitte-maven-jenkins-demo
     	//Jenkins URL: http://192.168.1.5:8080/job/deloitte-maven-jenkins-demo/
     	
     	System.out.println( "Hello World!" );
